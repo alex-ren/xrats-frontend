10e679cd20c2b55cb683af41cdb43b76eaada1d2
William Blair <wdblair@829llc.com>
2012-05-28 16:10:41 -0400

    Added readme

--- app.rb
+++ app.rb
@@ -4,16 +4,17 @@ $repos = []
 $repos << {name:"foo",home:"ats-0.2.7"}
 $repos << {name:"postiats",home:"ats-0.2.7"}
 
+
+
 def replace_pattern str, pattern, replace
   while str.match(pattern) do
     str.gsub!(pattern,replace)
   end
 end
 
-#Lets the web server send the file... will probably change later.
 def lxr_send_file path
-  response.headers['X-Accel-Redirect'] = "#{path}"
-  nil
+  text = File.open(Dir.pwd+path,"r").read
+  haml :regular_file,locals:{text:text}
 end
 
 def listing_of_directory directory
@@ -24,7 +25,7 @@ def listing_of_directory directory
   haml :directory_listing
 end
 
-def xref_of_file path
+def xref_of_file path, base
   raise Sinatra::NotFound if not File.exists?(path)
   file_folder = File.dirname(path)
   atsopt_path = "/opt/ats/bin/atsopt"
@@ -35,7 +36,7 @@ def xref_of_file path
   if path.match(/\.sats/)
     flag = "--static"
   end
-  atsopt = IO.popen("#{atsopt_path} --posmark_xref -IATS #{file_folder} #{flag} #{path}")
+  atsopt = IO.popen("#{atsopt_path} --posmark_xref -IATS #{base} -IATS #{file_folder} #{flag} #{path}")
   atsopt.readlines.join("")
 end
 
@@ -45,10 +46,7 @@ def make_xref repo_name, path
   end
   raise Sinatra::NotFound if (repo = repo.first).nil?
   ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{repo[:home]}"
-  absolute_path = "#{Dir.pwd}/repos/#{repo_name}/#{path}"
-  return listing_of_directory(absolute_path) if File.directory? absolute_path
-  return lxr_send_file "/repos/#{repo_name}/#{path}" if !path.match(/\.dats/) && !path.match(/\.sats/)
-  output = xref_of_file absolute_path
+  output = xref_of_file "repos/#{repo_name}/#{path}","repos/#{repo_name}"
   #Fix up all the links
   replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{repo[:home]}\/(.*)\"/,
                   "a href=\"/ats/#{repo[:home]}/\\1\"")
@@ -58,10 +56,7 @@ end
 
 def make_xref_ats_source home, path
   ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{home}"
-  absolute_path = "#{Dir.pwd}/ats/#{home}/#{path}"
-  return listing_of_directory(absolute_path) if File.directory? absolute_path
-  return lxr_send_file "/ats/#{home}/#{path}" if !path.match(/\.dats/) && !path.match(/\.sats/)
-  output = xref_of_file "#{Dir.pwd}/ats/#{home}/#{path}"
+  output = xref_of_file "#{Dir.pwd}/ats/#{home}/#{path}", ENV["ATSHOME"]
   replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{home}\/(.*)\"/,
                   "a href=\"/ats/#{home}/\\1\"")
   output
@@ -71,12 +66,24 @@ get '/' do
   haml :index
 end
 
-get '/ats/:home/*' do |home, path|
-  src = make_xref_ats_source home, path
-  haml :ats_source, locals:{source:src}
+get '/repos' do
+  haml :index
 end
 
-get '/:repo/*' do  |repo,path|
-  src = make_xref repo, path
+get '/ats/:home/*' do |home, path|
+  rel_path = "ats/#{home}/#{path}"
+  return listing_of_directory(rel_path) if File.directory? rel_path
+  return lxr_send_file "/ats/#{home}/#{path}" if !path.match(/\.dats/) && !path.match(/\.sats/)
+  src = make_xref_ats_source home, path
+  haml :ats_source, locals:{source:src,title:path,header:""}
+end
+
+get '/:repo/*' do  |repo_name,path|
+  rel_path = "repos/#{repo_name}/#{path}"
+  return listing_of_directory(rel_path) if File.directory? rel_path
+  if File.exists?(rel_path) && !path.match(/\.dats/) && !path.match(/\.sats/)
+    return lxr_send_file "/repos/#{repo_name}/#{path}"
+  end
+  src = make_xref repo_name, path
   haml :ats_source, locals:{source:src}
 end
