689ff9289685aa3b2f7196f0425aec00a05eb2a2
William Blair <wdblair@829llc.com>
2012-05-28 17:55:35 -0400

    Moved repo specification into YAML files

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require 'sinatra'
+require 'yaml'
 
-$repos = []
-$repos << {name:"foo",home:"ats-0.2.7"}
-$repos << {name:"postiats",home:"ats-0.2.7"}
-
-
+$repos = YAML.load_file("config/repos.yml")["repos"]
+$ats = YAML.load_file("config/ats.yml")["versions"]
 
 def replace_pattern str, pattern, replace
   while str.match(pattern) do
@@ -14,14 +12,14 @@ end
 
 def lxr_send_file path
   text = File.open(Dir.pwd+path,"r").read
-  haml :regular_file,locals:{text:text}
+  haml :regular_file, locals:{text:text}
 end
 
 def listing_of_directory directory
   raise Sinatra::NotFound if not File.directory?(directory)
   @directory = Dir.new(directory)
   @entries = @directory.entries
-  @entries.sort! 
+  @entries.sort!
   haml :directory_listing
 end
 
@@ -41,24 +39,29 @@ def xref_of_file path, base
 end
 
 def make_xref repo_name, path
-  repo = $repos.select do |r|
-    r[:name] == repo_name
+  repo = {}
+  $repos.each do |name,attr|
+    puts name
+    if name == repo_name
+      repo = attr
+      break
+    end
   end
-  raise Sinatra::NotFound if (repo = repo.first).nil?
-  ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{repo[:home]}"
+  raise Sinatra::NotFound if repo.empty?
+  ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{repo["ats"]}"
   output = xref_of_file "repos/#{repo_name}/#{path}","repos/#{repo_name}"
   #Fix up all the links
-  replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{repo[:home]}\/(.*)\"/,
-                  "a href=\"/ats/#{repo[:home]}/\\1\"")
+  replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{repo["ats"]}\/(.*)\"/,
+                  "a href=\"/ats/#{repo["ats"]}/\\1\"")
   replace_pattern(output,/a href=\"#{Dir.pwd}\/repos\/(.*)\"/,"a href=\"/\\1\"")
   output
 end
 
-def make_xref_ats_source home, path
-  ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{home}"
-  output = xref_of_file "#{Dir.pwd}/ats/#{home}/#{path}", ENV["ATSHOME"]
-  replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{home}\/(.*)\"/,
-                  "a href=\"/ats/#{home}/\\1\"")
+def make_xref_ats_source ats, path
+  ENV["ATSHOME"] = "#{Dir.pwd}/ats/#{ats}"
+  output = xref_of_file "#{Dir.pwd}/ats/#{ats}/#{path}", ENV["ATSHOME"]
+  replace_pattern(output,/a href\=\"#{Dir.pwd}\/ats\/#{ats}\/(.*)\"/,
+                  "a href=\"/ats/#{ats}/\\1\"")
   output
 end
 
@@ -70,11 +73,11 @@ get '/repos' do
   haml :index
 end
 
-get '/ats/:home/*' do |home, path|
-  rel_path = "ats/#{home}/#{path}"
+get '/ats/:home/*' do |ats, path|
+  rel_path = "ats/#{ats}/#{path}"
   return listing_of_directory(rel_path) if File.directory? rel_path
-  return lxr_send_file "/ats/#{home}/#{path}" if !path.match(/\.dats/) && !path.match(/\.sats/)
-  src = make_xref_ats_source home, path
+  return lxr_send_file rel_path if !path.match(/\.dats/) && !path.match(/\.sats/)
+  src = make_xref_ats_source ats, path
   haml :ats_source, locals:{source:src,title:path,header:""}
 end
 
